e8f328b79f69eb8227578ab49445f2e19cd7e859
twinkleg07 <tg4790775@gmail.com>
2026-01-31 17:47:10 +0000

    Add even_odd.py to check even or odd numbers
    This program checks if a given number is even or odd using the modulus operator.

---
+++ Basics/even_odd.py
@@ -0,0 +1,9 @@
+# Program: Even or Odd number
+# Author: Twinkle Gupta
+# Description: This program checks if given number is even or odd using modulus operator.
+
+x = int(input("Enter a number:"))
+if x % 2 == 0:
+  print(x,"is an even number")
+else:
+  print(x, "is an odd number")
