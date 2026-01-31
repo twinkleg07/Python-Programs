edef46dabb0d0186ea1976ab7322392bc2bd7de6
twinkleg07 <tg4790775@gmail.com>
2026-01-31 16:04:30 +0000

    Add Hello World program in hello.py
    Created my first Python program with basic print statement.

---
+++ Basics/hello.py
@@ -0,0 +1,4 @@
+# Program: Hello World
+# Author: Twinkle Gupta
+# Description: This program prints a simple Hello World message to mark the beginning of my Python learning journey.
+print("Hello, World! Welcome to my Python learning journey")
